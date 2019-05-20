bb3be4cc77d91571c0900ea56ceb510a7a65dbef
Rizky Adipratama <adipratama.rizky@gmail.com>
2019-05-20 08:21:58 +0700

    Initial Commit

---
+++ NC1ProjectFix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NC1ProjectFix
+//
+//  Created by Rizky Adipratama Ruddyar on 20/05/19.
+//  Copyright © 2019 Rizky Adipratama Ruddyar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
